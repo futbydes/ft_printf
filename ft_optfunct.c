4ad410dd7187af97702579a2b4297a251b5b5377
Vladislav LUDAN <vludan@e1r5p11.unit.ua>
2017-12-25 17:56:28 +0200

    1

--- ft_optfunct.c
+++ ft_optfunct.c
@@ -6,7 +6,7 @@
 /*   By: vludan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/11 11:55:42 by vludan            #+#    #+#             */
-/*   Updated: 2017/12/25 17:48:21 by vludan           ###   ########.fr       */
+/*   Updated: 2017/12/25 17:56:21 by vludan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,7 +93,7 @@ wchar_t			ft_unicon_conv(wchar_t c, t_flg *lst)
 	if (MB_CUR_MAX > 1 || (lst->type == 'S' || lst->type == 's'))
 	{
 	if ((int)c <= 127)
-		return (res = c);
+		return (res = (char)c);
 	else if ((int)c <= 2047)
 		return (res = ((c & 0x3F) | 0xC080) | ((c & 0x7C0) << 2));
 	else if ((int)c <= 65535)
@@ -103,5 +103,7 @@ wchar_t			ft_unicon_conv(wchar_t c, t_flg *lst)
 		return (res = ((((c & 0x3F) | 0xF0808080) | ((c & 0xFC0) << 2)) |
 					((c & 0x3F000) << 4) | ((c & 0xFC0000) << 3)));
 	}
+	else
+		res = (char)c;
 	return (c);
 }
