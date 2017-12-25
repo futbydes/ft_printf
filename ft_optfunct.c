551de93ad87572a799a1024e111e2cedc263043c
Vladislav LUDAN <vludan@e1r5p11.unit.ua>
2017-12-25 12:53:59 +0200

    1

--- ft_optfunct.c
+++ ft_optfunct.c
@@ -6,7 +6,7 @@
 /*   By: vludan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/11 11:55:42 by vludan            #+#    #+#             */
-/*   Updated: 2017/12/25 12:49:42 by vludan           ###   ########.fr       */
+/*   Updated: 2017/12/25 12:53:24 by vludan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,7 +70,7 @@ char			*ft_unicon_arr(t_or *u, t_flg *lst)
 	x = 1 + ((unsigned int)u->wct > 255) + ((unsigned int)u->wct > 65535) +
 		((unsigned int)u->wct > 16777215);
 	y = 0;
-	MB_CUR_MAX < x ? x = MB_CUR_MAX : 0;
+//	MB_CUR_MAX < x ? x = MB_CUR_MAX : 0;
 	arr = ft_memalloc(x);
 	if (lst->prc != -1)
 	{
@@ -89,12 +89,14 @@ wchar_t			ft_unicon_conv(wchar_t c)
 
 	if ((int)c <= 127)
 		return (res = c);
-	else if ((int)c <= 2047)
+	else if ((int)c <= 2047 && MB_CUR_MAX >= 2)
 		return (res = ((c & 0x3F) | 0xC080) | ((c & 0x7C0) << 2));
-	else if ((int)c <= 65535)
+	else if ((int)c <= 65535 && MB_CUR_MAX >= 3)
 		return (res = (((c & 0x3F) | 0xE08080) | ((c & 0xFC0) << 2)) |
 				((c & 0xF000) << 4));
-	else
+	else if (MB_CUR_MAX >= 4)
 		return (res = ((((c & 0x3F) | 0xF0808080) | ((c & 0xFC0) << 2)) |
 					((c & 0x3F000) << 4) | ((c & 0xFC0000) << 3)));
+	else
+		return (c);
 }
