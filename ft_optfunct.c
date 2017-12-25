0b5a5ce38ca5c8b798d1e267ba5bd10579868ce2
Vladislav LUDAN <vludan@e1r5p11.unit.ua>
2017-12-25 16:19:38 +0200

    1

--- ft_optfunct.c
+++ ft_optfunct.c
@@ -6,7 +6,7 @@
 /*   By: vludan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/11 11:55:42 by vludan            #+#    #+#             */
-/*   Updated: 2017/12/25 16:12:44 by vludan           ###   ########.fr       */
+/*   Updated: 2017/12/25 16:18:00 by vludan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,10 +43,13 @@ char			*ft_unicon(t_flg *lst, t_or *u)
 			u->wct = ft_unicon_conv(*(lst->awct)++, lst);
 			x = 1 + ((unsigned int)u->wct > 255) + ((unsigned int)u->wct >
 					65535) + ((unsigned int)u->wct > 16777215);
+//			if (MB_CUR_MAX < x)
+//				return (t);
 			t = ft_realloc(&(t), x, u);
-			if ((((temp = ft_unicon_arr(u, lst)) && *temp == 0)))
+			if ((temp = ft_unicon_arr(u, lst)) && *temp == 0)
 				return (t);
 			ft_memcpy(t + ft_strlen(t), temp, x);
+			free(temp);
 		}
 	}
 	if (lst->type == 'C' || lst->type == 'c')
