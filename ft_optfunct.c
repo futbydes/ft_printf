a0f71e4a196066b1b9d63e7a7ce57f1d66ef57be
Vladislav LUDAN <vludan@e3r4p9.unit.ua>
2017-12-26 16:28:19 +0200

    1

--- ft_optfunct.c
+++ ft_optfunct.c
@@ -6,7 +6,7 @@
 /*   By: vludan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/11 11:55:42 by vludan            #+#    #+#             */
-/*   Updated: 2017/12/26 16:10:40 by vludan           ###   ########.fr       */
+/*   Updated: 2017/12/26 16:27:46 by vludan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,7 @@ char			*ft_unicon_arr(t_or *u, t_flg *lst)
 	x = 1 + ((unsigned int)u->wct > 255) + ((unsigned int)u->wct > 65535) +
 		((unsigned int)u->wct > 16777215);
 	y = 0;
-	MB_CUR_MAX < x ? x = 1 : 0;
+	MB_CUR_MAX < x ? x = 0 : 0;
 	arr = ft_memalloc(x);
 	if ((lst->prc > -1 && (lst->type == 's' || lst->type == 'S')) ||
 		   	((lst->prc > 0 && lst->m_fw <= lst->prc) && (lst->type == 'C' ||
@@ -77,12 +77,8 @@ char			*ft_unicon_arr(t_or *u, t_flg *lst)
 		if (lst->prc < 0)
 			return (arr);
 	}
-	 while (x--)
+	while (x--)
 		arr[y++] = u->byte[x];
-//	 printf("%c", arr[0]);
-//	 printf("%c", arr[1]);
-//	 printf("%c\n", arr[2]);
-//	 printf("%d", arr[3]);
 	return (arr);
 }
 
@@ -93,7 +89,7 @@ wchar_t			ft_unicon_conv(wchar_t c, t_flg *lst)
 	res = 0;
 	if (MB_CUR_MAX > 1 || (lst->type == 'S' || lst->type == 's'))
 	{
-	if ((int)c <= 127)
+	if ((unsigned int)c <= 127)
 		return (res = (char)c);
 	else if ((int)c <= 2047)
 		return (res = ((c & 0x3F) | 0xC080) | ((c & 0x7C0) << 2));
