751e40c8373fa1c718c7b161acc3bfd066540014
Vladislav LUDAN <vludan@e1r5p13.unit.ua>
2018-02-11 17:15:49 +0200

    1

--- ft_printf.h
+++ ft_printf.h
@@ -19,6 +19,21 @@
 # include <wchar.h>
 # include <inttypes.h>
 
+# define ANSI_COLOR_RED     "\x1b[31m"
+# define ANSI_COLOR_GREEN   "\x1b[32m"
+# define ANSI_COLOR_YELLOW  "\x1b[33m"
+# define ANSI_COLOR_BLUE    "\x1b[34m"
+# define ANSI_COLOR_MAGENTA "\x1b[35m"
+# define ANSI_COLOR_CYAN    "\x1b[36m"
+# define ANSI_COLOR_RESET   "\x1b[0m"
+# define ANSI_BRIGHT_BLACK	"\x1b[90m"
+# define ANSI_BRIGHT_RED	"\x1b[91m"
+# define ANSI_BRIGHT_GREEN	"\x1b[92m"
+# define ANSI_BRIGHT_YELLOW	"\x1b[93m"
+# define ANSI_BRIGHT_BLUE	"\x1b[94m"
+# define ANSI_BRIGHT_MAGENT	"\x1b[95m"
+# define ANSI_BRIGHT_CYAN	"\x1b[96m"
+
 typedef struct		s_flags
 {
 	char			plus;
